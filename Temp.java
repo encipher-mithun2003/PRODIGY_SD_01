3213abded3819b06c32844ccaf7ac445fe9154b6
Mithun Varshan S <114509306+encipher-mithun2003@users.noreply.github.com>
2024-04-20 22:11:58 +0530

    Temp.java

--- Temp.java
+++ Temp.java
@@ -1,50 +1,32 @@
 import java.util.Scanner;
-
 public class Temp{
-
-    // Function to convert Celsius to Fahrenheit
     public static double celsiusToFahrenheit(double celsius) {
         return (celsius * 9 / 5) + 32;
     }
-
-    // Function to convert Celsius to Kelvin
     public static double celsiusToKelvin(double celsius) {
         return celsius + 273.15;
     }
-
-    // Function to convert Fahrenheit to Celsius
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
-
-    // Function to convert Fahrenheit to Kelvin
     public static double fahrenheitToKelvin(double fahrenheit) {
         return (fahrenheit + 459.67) * 5 / 9;
     }
-
-    // Function to convert Kelvin to Celsius
     public static double kelvinToCelsius(double kelvin) {
         return kelvin - 273.15;
     }
-
-    // Function to convert Kelvin to Fahrenheit
     public static double kelvinToFahrenheit(double kelvin) {
         return kelvin * 9 / 5 - 459.67;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Temperature Conversion!\n");
-
         System.out.print("Enter the temperature value: ");
         double temperature = scanner.nextDouble();
-
         System.out.print("Enter the original unit of measurement (Celsius, Fahrenheit, or Kelvin): ");
         String originalUnit = scanner.next().toLowerCase();
-
         double convertedValue1 = 0, convertedValue2 = 0;
-
         if (originalUnit.equals("celsius")) {
             convertedValue1 = celsiusToFahrenheit(temperature);
             convertedValue2 = celsiusToKelvin(temperature);
